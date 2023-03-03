a15799621cb893a0f6ed881afac082b5caf59e34
danjovich <78160019+danjovich@users.noreply.github.com>
2023-03-03 02:09:39 -0300

    feat: update repo through -u or --update flags

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
 from Pomodoro import Pomodoro
 from read_single_keypress import read_single_keypress
 from clearNLines import clearNLines
+from argparse import ArgumentParser
+from os import path
+import subprocess
+
+parser = ArgumentParser(
+    prog='Pomodoro CLI',
+    description='A simple CLI thats serves as a minimal interface for the pomodoro concentration technique'
+)
+parser.add_argument('-u', '--update', action='store_true', help='update the app')
+args = parser.parse_args()
+
+if args.update:
+    print('Updating Happiness Tracker!')
+    dirName = path.realpath(path.dirname(__file__))
+    subprocess.call(['bash', f'{dirName}/../scripts/update.sh'])
+    exit()
 
 print('Pomodoro CLI 🍅')
 
