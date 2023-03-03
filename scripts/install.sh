725eab9d59f28b0208b09f677cc8d26602affee6
danjovich <78160019+danjovich@users.noreply.github.com>
2023-03-03 02:10:01 -0300

    refactor: install as pomodoro instead of pomodoro-cli

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,6 @@ if [ $? -ne 0 ]; then
 fi
 cd pomodoro-cli
 ARG='\$1'
-sudo sh -c "echo \"#!/usr/bin/sh\npython3 $(pwd)/src/main.py $ARG\" > /usr/local/bin/pomodoro-cli"
-sudo sh -c "chmod +x /usr/local/bin/pomodoro-cli"
-echo "Happiness Tracker installed! You can run it in a shell with the \"pomodoro-cli\" command"
+sudo sh -c "echo \"#!/usr/bin/sh\npython3 $(pwd)/src/main.py $ARG\" > /usr/local/bin/pomodoro"
+sudo sh -c "chmod +x /usr/local/bin/pomodoro"
+echo "Happiness Tracker installed! You can run it in a shell with the \"pomodoro\" command"
