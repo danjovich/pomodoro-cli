dadb628c8906461f87dbbabd44af175f42175731
danjovich <78160019+danjovich@users.noreply.github.com>
2023-03-04 00:43:48 -0300

    fix: install.sh generated a bin file that couldn't pass more than one cl argument

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 cd pomodoro-cli
-ARG='\$1'
+ARG='\$@'
 sudo sh -c "echo \"#!/usr/bin/sh\npython3 $(pwd)/src/main.py $ARG\" > /usr/local/bin/pomodoro"
 sudo sh -c "chmod +x /usr/local/bin/pomodoro"
 echo "Pomodoro CLI installed! You can run it in a shell with the \"pomodoro\" command"
