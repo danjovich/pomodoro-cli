88d9dc1cb54c1d8c5766f8fa5faef5c0f9754c0e
danjovich <78160019+danjovich@users.noreply.github.com>
2023-09-16 18:15:41 -0300

    fix: update scripts and add requirements.txt to avoid pip errors in more recent apt versions

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,21 @@
+# clones the repo and enters it
 git clone https://github.com/danjovich/pomodoro-cli
-pip install readchar
+cd pomodoro-cli
+
+# handles python venv to install the proper packages
+python3 -m venv .venv                                                                                      
+.venv/bin/python3 -m pip install -r requirements.txt
+
+# checks for errors in the previous commands and exits if it finds anyt
 if [ $? -ne 0 ]; then
   exit 1
 fi
-cd pomodoro-cli
+
+# adds a bash file to /usr/local/bin to make it available as a terminal command
 ARG='\$@'
-sudo sh -c "echo \"#!/usr/bin/sh\npython3 $(pwd)/src/main.py $ARG\" > /usr/local/bin/pomodoro"
+sudo sh -c "echo \"#!/usr/bin/sh
+$(pwd)/.venv/bin/python3 $(pwd)/src/main.py $ARG\" > /usr/local/bin/pomodoro"
 sudo sh -c "chmod +x /usr/local/bin/pomodoro"
+
+# success message
 echo "Pomodoro CLI installed! You can run it in a shell with the \"pomodoro\" command"
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/.."
 git pull
+.venv/bin/python3 -m pip install -r requirements.txt
